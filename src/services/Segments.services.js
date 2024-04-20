b777aca8786bbd03d70fbbf17d84658d737d5de7
hamzabilalbaig <hamzabilalbaig@gmail.com>
2024-04-21 00:15:51 +0500

    ..

--- src/services/Segments.services.js
+++ src/services/Segments.services.js
@@ -3,13 +3,17 @@ const { Op } = require("sequelize");
 
 async function createSegment(UserID, segment) {
   try {
-    let result = segment;
-    if (result?.GroupID) {
-      result = await sequelizeServer?.models?.Segments.findOne({
-        where: {
-          GroupID: result?.GroupID,
-        },
-      });
+    let result = [];
+    if (segment?.GroupID?.length > 0) {
+      for (const GropID of segment?.GroupID) {
+        let r = await sequelizeServer?.models?.Segments.findOne({
+          where: {
+            GroupID: GropID,
+          },
+        });
+
+        result?.push(r);
+      }
     } else {
       result = await sequelizeServer.models.Segments.create({
         UserID: UserID,
@@ -18,21 +22,44 @@ async function createSegment(UserID, segment) {
       });
     }
 
-    if (segment?.products && segment.products.length > 0) {
-      const productIds = segment.products.map((data) => data.productID);
-      for (const productId of productIds) {
-        const existingEntry =
-          await sequelizeServer.models.Segment_Products.findOne({
-            where: {
+    if (result?.length > 0) {
+      for (const res of result) {
+        if (segment?.products && segment.products.length > 0) {
+          const productIds = segment.products.map((data) => data.productID);
+          for (const productId of productIds) {
+            const existingEntry =
+              await sequelizeServer.models.Segment_Products.findOne({
+                where: {
+                  GroupID: res?.GroupID,
+                  ProductID: productId,
+                },
+              });
+            if (!existingEntry) {
+              await sequelizeServer?.models?.Segment_Products?.create({
+                GroupID: res?.GroupID,
+                ProductID: productId,
+              });
+            }
+          }
+        }
+      }
+    } else {
+      if (segment?.products && segment.products.length > 0) {
+        const productIds = segment.products.map((data) => data.productID);
+        for (const productId of productIds) {
+          const existingEntry =
+            await sequelizeServer.models.Segment_Products.findOne({
+              where: {
+                GroupID: result?.GroupID,
+                ProductID: productId,
+              },
+            });
+          if (!existingEntry) {
+            await sequelizeServer?.models?.Segment_Products?.create({
               GroupID: result?.GroupID,
               ProductID: productId,
-            },
-          });
-        if (!existingEntry) {
-          await sequelizeServer?.models?.Segment_Products?.create({
-            GroupID: result?.GroupID,
-            ProductID: productId,
-          });
+            });
+          }
         }
       }
     }
